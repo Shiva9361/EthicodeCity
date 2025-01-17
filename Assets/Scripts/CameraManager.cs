13212ac51d337a1537759cc8f8470a298561fa07
Shiva9361 <shivadharshansankar936@gmail.com>
2025-01-17 22:17:55 +0530

    feat: camera zoom in and out

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    public float dragSpeed = 2f; // Speed of the camera movement
+    private float dragSpeed = 2f; // Speed of the camera movement
+    public float dragRatio = 5; // Ratio of the camera movement speed to the orthographic size
     private Vector3 dragOrigin; // Stores the initial mouse position
     private bool isDragging = false;
 
@@ -11,8 +12,18 @@ public class CameraManager : MonoBehaviour
     void Update()
     {
         HandleCameraDrag();
+        HandleZoom();
     }
 
+    void HandleZoom()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll != 0 && cameraDragEnabled)
+        {
+            Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - scroll * 2, 2, 7);
+            dragSpeed = Camera.main.orthographicSize / dragRatio;
+        }
+    }
     void HandleCameraDrag()
     {
         if (Input.GetMouseButtonDown(0) && cameraDragEnabled) // Left mouse button pressed
