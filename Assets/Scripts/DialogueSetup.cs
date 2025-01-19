a0a89f2340a2584ba09178dd3ee886087c7b25c6
Akileshdash <cs22b040@iittp.ac.in>
2025-01-19 23:36:53 +0530

    Fixed house images

--- Assets/Scripts/DialogueSetup.cs
+++ Assets/Scripts/DialogueSetup.cs
@@ -17,49 +17,52 @@ public class DialogueSetup : MonoBehaviour
         // Alex
         dialogueManager.characters[0] = new Character()
         {
-            name = "Alex",
-            image = alexImage,
+            name = "Jhon",
+            image = johnImage,
             dialogueLines = new string[] {
-                "Hey, I'm Alex.",
-                "It's a beautiful day, isn't it?",
-                "Let's go on an adventure!"
+                "Hey, I'm John ,the architect of the city.",
+                // "It's a beautiful day, isn't it?",
+                // "Let's go on an adventure!",
+                "You are the new Mayor of this city and must rebuild it after unforeseen circumstances caused its collapse.",
+                "The previous mayor has been arrested, and it’s now up to you to restore the city and uncover the reasons for its downfall.",
+                " Let's start from scratch and rebuild a stronger, better city!",
             }
         };
 
         // Elena
-        dialogueManager.characters[1] = new Character()
-        {
-            name = "Elena",
-            image = elenaImage,
-            dialogueLines = new string[] {
-                "Hello, I'm Elena.",
-                "I love exploring new places.",
-                "Ready to join me?"
-            }
-        };
+        // dialogueManager.characters[1] = new Character()
+        // {
+        //     name = "Elena",
+        //     image = elenaImage,
+        //     dialogueLines = new string[] {
+        //         "Hello, I'm Elena.",
+        //         "I love exploring new places.",
+        //         "Ready to join me?"
+        //     }
+        // };
 
         // Sam
-        dialogueManager.characters[2] = new Character()
-        {
-            name = "Sam",
-            image = samImage,
-            dialogueLines = new string[] {
-                "Yo, it's Sam here!",
-                "I'm all for some action.",
-                "Count me in!"
-            }
-        };
+        // dialogueManager.characters[2] = new Character()
+        // {
+        //     name = "Sam",
+        //     image = samImage,
+        //     dialogueLines = new string[] {
+        //         "Yo, it's Sam here!",
+        //         "I'm all for some action.",
+        //         "Count me in!"
+        //     }
+        // };
 
         // John
-        dialogueManager.characters[3] = new Character()
-        {
-            name = "John",
-            image = johnImage,
-            dialogueLines = new string[] {
-                "Hi, I'm John.",
-                "I'm excited to be here.",
-                "Let's make this an unforgettable trip!"
-            }
-        };
+        // dialogueManager.characters[3] = new Character()
+        // {
+        //     name = "John",
+        //     image = johnImage,
+        //     dialogueLines = new string[] {
+        //         "Hi, I'm John.",
+        //         "I'm excited to be here.",
+        //         "Let's make this an unforgettable trip!"
+        //     }
+        // };
     }
 }
