dd34102312751ee487aef167d03d345b9ce3ab85
Akileshdash <cs22b040@iittp.ac.in>
2025-01-20 02:22:43 +0530

    Added space for AI Credits in the house panel

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 public class InventoryManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public int Population;
+    public int AiCredits;
     public float Money;
 
     public Text moneyText;
