750b428a31009d146789d238ff4eb842f103c70a
Shiva9361 <shivadharshansankar936@gmail.com>
2025-01-19 18:37:02 +0530

    fix: camera movement

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -8,6 +8,8 @@ public class CameraManager : MonoBehaviour
     private Vector3 dragOrigin; // Stores the initial mouse position
     private bool isDragging = false;
 
+    public GameObject parent;
+
     public Button[] disableButtons;
 
     public Button[] enableButtons;
@@ -59,9 +61,11 @@ public class CameraManager : MonoBehaviour
             Vector3 currentMousePosition = Input.mousePosition;
             Vector3 difference = dragOrigin - currentMousePosition;
 
-            // Move the camera based on the difference
+            // Convert the difference to world space considering the camera's orientation
             Vector3 move = new Vector3(difference.x, 0, difference.y) * dragSpeed * Time.deltaTime;
-            transform.Translate(move, Space.World);
+            move = parent.transform.TransformDirection(move);
+
+            parent.transform.Translate(move, Space.World);
 
             // Update drag origin
             dragOrigin = currentMousePosition;
