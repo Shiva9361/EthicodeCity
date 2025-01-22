7cd41ac841e028a117eca13a7d3a9795bbf681c3
i-apex <jaim7viramgama@gmail.com>
2025-01-22 15:01:11 +0530

    Questionarie Scene

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,85 +1,60 @@
-using System.Collections;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
-    public TextMeshProUGUI nameText;
-    public TextMeshProUGUI dialogueText;
-    public Image characterImage;
-    public float textSpeed;
-    public Character[] characters;  // Array of 4 characters (Alex, Elena, Sam, John)
+    public Image questionImage;           // UI Image to display the question
+    public TMP_InputField inputField;     // Input field for user answers
+    public Button submitButton;           // Submit button
+    public Sprite[] questionImages;       // Array of question images
 
-    private int currentLineIndex = 0;
-    private int currentCharacterIndex = 0;
+    private int currentQuestionIndex = 0;
 
     void Start()
     {
-        StartDialogue();
+        DisplayQuestion();
+        submitButton.onClick.AddListener(HandleSubmit);
     }
 
-    void Update()
+    void DisplayQuestion()
     {
-        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.RightArrow))
+        if (currentQuestionIndex < questionImages.Length)
         {
-            if (dialogueText.text == characters[currentCharacterIndex].dialogueLines[currentLineIndex])
-            {
-                NextLine();
-            }
-            else
-            {
-                StopAllCoroutines();
-                dialogueText.text = characters[currentCharacterIndex].dialogueLines[currentLineIndex];
-            }
-        }
-    }
-
-    void StartDialogue()
-    {
-        currentLineIndex = 0;
-        currentCharacterIndex = 0;
-        DisplayCharacterDialogue();
-    }
-
-    void DisplayCharacterDialogue()
-    {
-        nameText.text = characters[currentCharacterIndex].name;
-        characterImage.sprite = characters[currentCharacterIndex].image;
-        dialogueText.text = string.Empty;
-
-        StartCoroutine(TypeLine());
-    }
-
-    IEnumerator TypeLine()
-    {
-        foreach (char c in characters[currentCharacterIndex].dialogueLines[currentLineIndex].ToCharArray())
-        {
-            dialogueText.text += c;
-            yield return new WaitForSeconds(textSpeed);
-        }
-    }
-
-    void NextLine()
-    {
-        if (currentLineIndex < characters[currentCharacterIndex].dialogueLines.Length - 1)
-        {
-            currentLineIndex++;
-            dialogueText.text = string.Empty;
-            StartCoroutine(TypeLine());
+            questionImage.sprite = questionImages[currentQuestionIndex]; // Update the image
+            questionImage.enabled = true;  // Make sure the image is visible
+            inputField.text = "";          // Clear the input field
         }
         else
         {
-            currentCharacterIndex++;
-            if (currentCharacterIndex < characters.Length)
-            {
-                currentLineIndex = 0;
-                DisplayCharacterDialogue();
-            }
-            else
-            {
-                gameObject.SetActive(false);  // End the dialogue
-            }
+            EndQuiz(); // End the quiz if there are no more questions
         }
+    }
+
+    void HandleSubmit()
+    {
+        string userAnswer = inputField.text;
+
+        // Validate user input
+        if (!string.IsNullOrWhiteSpace(userAnswer))
+        {
+            Debug.Log($"Answer for Question {currentQuestionIndex + 1}: {userAnswer}");
+
+            // Move to the next question
+            currentQuestionIndex++;
+            DisplayQuestion();
+        }
+        else
+        {
+            Debug.Log("Please enter an answer before submitting.");
+        }
+    }
+
+    void EndQuiz()
+    {
+        Debug.Log("Quiz Completed!");
+        questionImage.gameObject.SetActive(false);  // Hide the image
+        inputField.gameObject.SetActive(false);     // Hide the input field
+        submitButton.gameObject.SetActive(false);   // Hide the submit button
     }
 }
